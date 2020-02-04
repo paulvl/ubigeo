8e38166735cd02952e4170f4b5b10d133219fd02
Paul Vidal <pvidal@Mac-mini-de-Paul.local>
2020-02-04 03:33:56 -0500

    WIP: first commit

---
+++ src/Department.php
@@ -0,0 +1,41 @@
+<?php
+
+class Department
+{
+	private $data = [
+		['id' => '01', 'name' => 'AMAZONAS'],
+		['id' => '02', 'name' => 'ANCASH'],
+		['id' => '03', 'name' => 'APURIMAC'],
+		['id' => '04', 'name' => 'AREQUIPA'],
+		['id' => '05', 'name' => 'AYACUCHO'],
+		['id' => '06', 'name' => 'CAJAMARCA'],
+		['id' => '07', 'name' => 'CALLAO'],
+		['id' => '08', 'name' => 'CUSCO'],
+		['id' => '09', 'name' => 'HUANCAVELICA'],
+		['id' => '10', 'name' => 'HUANUCO'],
+		['id' => '11', 'name' => 'ICA'],
+		['id' => '12', 'name' => 'JUNIN'],
+		['id' => '13', 'name' => 'LA LIBERTAD'],
+		['id' => '14', 'name' => 'LAMBAYEQUE'],
+		['id' => '15', 'name' => 'LIMA'],
+		['id' => '16', 'name' => 'LORETO'],
+		['id' => '17', 'name' => 'MADRE DE DIOS'],
+		['id' => '18', 'name' => 'MOQUEGUA'],
+		['id' => '19', 'name' => 'PASCO'],
+		['id' => '20', 'name' => 'PIURA'],
+		['id' => '21', 'name' => 'PUNO'],
+		['id' => '22', 'name' => 'SAN MARTIN'],
+		['id' => '23', 'name' => 'TACNA'],
+		['id' => '24', 'name' => 'TUMBES'],
+		['id' => '25', 'name' => 'UCAYALI']
+	];
+	/*
+	function where($key, $value, $data) {
+		$filter = array_filter($data, function ($element) use ($key, $value) {
+	  		return $element[$key] == $value;
+		});
+		sort($filter);
+		return $filter;
+	}
+	*/
+}
